0d905a3e57b7426652d38f692aaeee29ebdcb23a
binisforbinary <binisforbinary@gmail.com>
2014-10-29 23:54:11 +0800

    add Vec.h for chapter 11

---
+++ chapter 11/Vec.h
@@ -0,0 +1,74 @@
+
+
+
+
+template <class T> class Vec {
+public:
+	typedef T* iterator;
+	typedef const T* const_iterator;
+	typedef size_t size_type;
+	typedef T value_type;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef std::ptrdiff_t difference_type;
+
+	// interface
+	Vec() { create(); }
+	explicit Vec(size_type n, const T& val = T()) { create(n, val); }
+
+	// index and size
+	T& operator[](size_type i) { return data[i]; }
+	const T& operator[] (size_type i) const { return data[i]; }
+	size_type size() const { return avail - data; }
+
+	// operations that return iterators
+	iterator begin() { return data; }
+	const_iterator begin() const { return data; }
+	iterator end() { return avail; }
+	const_iterator end() const { return avail; }
+
+	// copy control
+	Vec(const Vec& v) {
+		create(v.begin(), v.end());
+	}
+
+	// assignment
+	Vec& operator=(const Vec& v) {
+		if(&v != this) {
+			uncreate();
+			create(v.begin(), v.end());
+		}
+		return *this;
+	}
+
+	// destructor
+	~Vec() { uncreate(); }
+
+	// push back
+	void push_back(const T& t) {
+		if(avail == limit) 
+			grow();
+		unchecked_append(val);
+	}
+
+private:
+	iterator data;
+	iterator avail;
+	iterator limit;	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
